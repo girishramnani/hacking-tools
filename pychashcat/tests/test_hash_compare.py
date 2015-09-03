e837034a69f16c0e68240eded898270f00403e04
girishramnani <girishramnani95@gmail.com>
2015-09-03 23:37:58 +0530

    added stub

---
+++ pychashcat/tests/test_hash_compare.py
@@ -0,0 +1,25 @@
+__author__ = 'girish'
+
+
+
+class standard_hash_test(object):
+
+    def __init__(self,Algorithm):
+        self.algorithm = Algorithm
+
+
+    def test_small_hash_compare(self):
+        pass
+
+
+    def test_all_number_hash_compare(self):
+        pass
+
+    def test_all_alphabet_hash_compare(self):
+        pass
+
+
+    def test_all_special_charactor_hash_compare(self):
+        pass
+
+
