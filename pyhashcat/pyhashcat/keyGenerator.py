fb5993d73daaafc457dd34eef33dcca5a163fb7a
girishramnani <girishramnani95@gmail.com>
2015-09-04 00:31:00 +0530

    added some documentation

--- pyhashcat/pyhashcat/keyGenerator.py
+++ pyhashcat/pyhashcat/keyGenerator.py
@@ -2,15 +2,29 @@ from abc import ABCMeta
 
 __author__ = 'girish'
 
+"""
+Key generator provides different patterns and options to reduce the keyword search space.
+The ways to provide control over the keywords are:
 
+:param pattern -> accepts a regex like language to match pattern.
+        "?" -> matching a single character
+       length -> to provide the length of the keyword
+       min-length,max-length -> provide min-max length of he keyword (will work only if length and pattern are not given)
+       has_num,has_characters, has_symbols -> flags which will be true by default , can be set to control the search domain.
+
+Key generator can also take a file as input which will work as a dictionary.
+"""
 
 class AbstractKeyGenerator(metaclass=ABCMeta):
-
+    """
+    Use this class to implement you custom generator for more fine grain control over the
+    keywords
+    """
 
     def __init__(self,pattern=None):
         self.pattern = pattern
 
-    def gen_keywords(self,length):
+    def gen_keywords(self,length,*args,**kwargs):
         raise NotImplementedError
 
 
--- pyhashcat/tests/test_key_generator.py
+++ pyhashcat/tests/test_key_generator.py
@@ -2,10 +2,16 @@ from unittest.case import TestCase
 
 __author__ = 'girish'
 
-
+from pyhashcat.keyGenerator import AbstractKeyGenerator
 
 class NumericalGeneratorTestCase(TestCase):
 
 
     def setUp(self):
-        
+        self.small_pattern = "???"
+        self.medium_pattern = "h??l? w??l?"
+        self.num_pattern ="1??"
+
+    def test_only_numerical_generator(self):
+        pass
+
