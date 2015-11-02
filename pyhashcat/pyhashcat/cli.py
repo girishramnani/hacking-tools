50d68cb43e470c20e416d2977eba981fda2f8bda
girishramnani <girishramnani95@gmail.com>
2015-11-02 23:46:30 +0530

    added input_group

--- pyhashcat/pyhashcat/cli.py
+++ pyhashcat/pyhashcat/cli.py
@@ -9,8 +9,17 @@ def parse_args(args=[]):
     algorithm_group.add_argument("--sha1",action="store_true",default=False)
     algorithm_group.add_argument("--sha256",action="store_true",default=False)
 
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument("-n","--num",metavar='N',type=int,help="only use number having N length")
+    input_group.add_argument("-c","--chars",metavar="N",type=int,help="only use characters having N length")
+    input_group.add_argument("-b","--both",metavar="N",type=int,help="use both having N length")
+    input_group.add_argument("-r","--regex",metavar="N",type=str,help="use a regex for the desired query set (beta) ")
 
 
 
     return parser
 
+parser =parse_args()
+args =parser.parse_args(["--sha1","-c","6","girsih"])
+
+print(args)
