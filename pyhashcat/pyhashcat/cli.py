4d70fd60d481852674eca7e7f7a9abd3feabaea4
girishramnani <girishramnani95@gmail.com>
2015-11-02 23:36:56 +0530

    created test and added the functionality

--- pyhashcat/pyhashcat/cli.py
+++ pyhashcat/pyhashcat/cli.py
@@ -1,1 +1,16 @@
 __author__ = 'girish'
+import argparse
+
+def parse_args(args=[]):
+    parser = argparse.ArgumentParser(description="reverse hashes of passwords")
+    parser.add_argument('hash',type=str,help="The hash you want to reverse")
+    algorithm_group = parser.add_mutually_exclusive_group(required=True)
+    algorithm_group.add_argument("--md5",action="store_true",default=False)
+    algorithm_group.add_argument("--sha1",action="store_true",default=False)
+    algorithm_group.add_argument("--sha256",action="store_true",default=False)
+
+
+
+
+    return parser
+
--- pyhashcat/tests/test_command_line.py
+++ pyhashcat/tests/test_command_line.py
@@ -1,1 +1,22 @@
 __author__ = 'girish'
+
+import unittest
+import cli
+
+class commandLineTest(unittest.TestCase):
+
+
+    def test_algorithm_group(self):
+        parser = cli.parse_args()
+
+        self.assertEqual(parser.parse_args(["--sha1","hash"]).sha1,True)
+
+
+    def test_no_algorithm_group(self):
+        parser = cli.parse_args()
+        try:
+
+            self.assertRaises(parser.parse_args(["hash"]),lambda :SystemExit)
+        except SystemExit:
+            self.assertEqual(True,True) #cant find other way to check system exit
+
